817a770513e159b5130c57a617177bce15e5d96e
y6692 <y6692@126.com>
2019-04-16 10:53:04 +0800

    git路经修改版完善5

--- app/src/main/java/cn/qimate/test/activity/MainActivity.java
+++ app/src/main/java/cn/qimate/test/activity/MainActivity.java
@@ -289,8 +289,8 @@ public class MainActivity extends Activity implements OnClickListener, LocationS
         bikeMarkerList = new ArrayList<>();
         imageWith = (int) (getWindowManager().getDefaultDisplay().getWidth() * 0.8);
 
-        mapView = findViewById(R.id.mainUI_map);
-        mapView.onCreate(savedInstanceState);
+//        mapView = findViewById(R.id.mainUI_map);
+//        mapView.onCreate(savedInstanceState);
 
 //        if (aMap == null) {
 //            aMap = mapView.getMap();
